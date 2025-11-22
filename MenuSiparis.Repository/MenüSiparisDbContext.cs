c7f0a0ace4e0b381f4dd64420b5519e6171615e6
Can Onay <arifonay.853@gmail.com>
2025-11-22 23:40:05 +0300

    Context sınıfı oluşturulup api katmanında IOC contenirına kayıt edildi.İlk migration dosyası başarılı bir şekilde oluşturuldu

---
+++ MenuSiparis.Repository/MenüSiparisDbContext.cs
@@ -0,0 +1,30 @@
+﻿using MenuSiparis.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuSiparis.Repository
+{
+    public class MenüSiparisDbContext(DbContextOptions<MenüSiparisDbContext>options):DbContext(options)
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<SocialMedia> SocialMedias { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
